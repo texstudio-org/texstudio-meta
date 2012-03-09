9df4d973d22f1e0b1a9b231de2e2e5b8824b6a2e
benibela <benito@benibela.de>
2012-03-09 23:47:32 +0000

    check that all places mention the same txs version
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@2638 92cce386-e96d-4106-809f-596c673a3fd8

--- createtag_srctar.sh
+++ createtag_srctar.sh
@@ -9,10 +9,28 @@ echo "create svn tag and src tarball"
 TMXREVISION=`svn info | grep Revision | grep -oE [0-9]+`
 echo "Assuming i should tag revision $TMXREVISION"
 
+TXS_VERSION_CPP=`grep TXSVERSION smallUsefulFunctions.h | head -1 | cut -d " " -f 3 | tr -d "\"" | grep -oE [0-9.]+`
+TXS_VERSION_MANPAGE=`grep TH utilities/texstudio.1 | head -1 | cut -d " " -f 5 | tr -d "\""`
+TXS_VERSION_DEBIAN_CHANGELOG=`head -1 debian/changelog | grep -oE [0-9][.][0-9] `
+TXS_VERSION_ISS1=`grep AppVer utilities/notInBinary/texmakerx.iss | head -1 | grep -oE [0-9][.][0-9]`
+TXS_VERSION_ISS2=`grep AppVer utilities/notInBinary/texmakerx.iss | head -1 | grep -oE [0-9][.][0-9]`
+TXS_VERSION_MANUAL=`grep SECTIONNEW utilities/usermanual_en.html | tail -1 | grep -oE "Version +[0-9][.][0-9]" | tail -1 | grep -oE [0-9][.][0-9]`
 
-echo "Enter new txs version:"
-read TMXVERSION
+if [[ "$TXS_VERSION_CPP" != "$TXS_VERSION_MANPAGE" ]]; 
+then echo Error: Version mismatch: manpage: -$TXS_VERSION_MANPAGE- cpp: -$TXS_VERSION_CPP-; 
+exit; fi
+if [[ $TXS_VERSION_CPP != $TXS_VERSION_DEBIAN_CHANGELOG ]]; then echo Error: Version mismatch: changelog: $TXS_VERSION_DEBIAN_CHANGELOG cpp: $TXS_VERSION_CPP; exit; fi
+if [[ $TXS_VERSION_CPP != $TXS_VERSION_ISS1 ]]; then echo Error: Version mismatch: iss: $TXS_VERSION_ISS1 cpp: $TXS_VERSION_CPP; exit; fi
+if [[ $TXS_VERSION_CPP != $TXS_VERSION_ISS2 ]]; then echo Error: Version mismatch: iss 2: $TXS_VERSION_ISS2 cpp: $TXS_VERSION_CPP; exit; fi
+if [[ $TXS_VERSION_CPP != $TXS_VERSION_MANUAL ]]; then echo Error: Version mismatch: manual: $TXS_VERSION_MANUAL cpp: $TXS_VERSION_CPP; exit; fi
 
+
+
+#echo "Enter new txs version:"
+#read TMXVERSION
+TMXVERSION=$TXS_VERSION_CPP
+
+echo "Publishing version $TMXVERSION"
 
 SFUSER="";
 if [ -e /home/benito ]; then SFUSER="benibela"; else echo "Enter sourceforge user name:"; read SFUSER; fi
