3d2ba892af25edb1b0f947118cc60c16605b78e9
benibela <benito@benibela.de>
2012-08-04 17:30:29 +0000

    new txsUpdate.xml format
    git-svn-id: svn+ssh://svn.code.sf.net/p/texstudio/code/branches/web@3095 92cce386-e96d-4106-809f-596c673a3fd8

--- webpage/upload.sh
+++ webpage/upload.sh
@@ -4,6 +4,6 @@ SFUSER="";
 if [ -e /home/benito ]; then SFUSER="benibela"; else echo "Enter sourceforge user name:"; read SFUSER; fi
 if [ "x$SFUSER"  = "x" ]; then echo "no sf user"; exit; fi
 
-rsync -v -e ssh index.html "$SFUSER,texmakerx@web.sourceforge.net:/home/groups/t/te/texmakerx/htdocs"
+rsync -v -e ssh index.html "$SFUSER,texstudio@web.sourceforge.net:/home/groups/t/te/texstudio/htdocs"
 
 svn commit -m "web page $@" index.html 
