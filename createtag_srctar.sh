f320f7b57312502fa4fb1932f5bdb10f0185bf00
benibela <benito@benibela.de>
2012-10-16 22:16:58 +0000

    update versions numbers
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@3377 92cce386-e96d-4106-809f-596c673a3fd8

--- createtag_srctar.sh
+++ createtag_srctar.sh
@@ -17,9 +17,9 @@ TXS_VERSION_ISS2=`grep AppVer utilities/notInBinary/texmakerx.iss | head -1 | gr
 TXS_VERSION_MANUAL=`grep SECTIONNEW utilities/usermanual_en.html | head -2 | tail -1 | grep -oE "Version +[0-9][.][0-9]" | tail -1 | grep -oE [0-9][.][0-9]`
 
 if [[ "$TXS_VERSION_CPP" != "$TXS_VERSION_MANPAGE" ]]; 
-then echo Error: Version mismatch: manpage: -$TXS_VERSION_MANPAGE- cpp: -$TXS_VERSION_CPP-; 
+then echo Error: Version mismatch: "manpage (utilities/texstudio.1):" -$TXS_VERSION_MANPAGE- cpp: -$TXS_VERSION_CPP-; 
 exit; fi
-if [[ $TXS_VERSION_CPP != $TXS_VERSION_DEBIAN_CHANGELOG ]]; then echo Error: Version mismatch: changelog: $TXS_VERSION_DEBIAN_CHANGELOG cpp: $TXS_VERSION_CPP; exit; fi
+if [[ $TXS_VERSION_CPP != $TXS_VERSION_DEBIAN_CHANGELOG ]]; then echo Error: "Version mismatch: changelog (debian/changelog):" $TXS_VERSION_DEBIAN_CHANGELOG cpp: $TXS_VERSION_CPP; exit; fi
 if [[ $TXS_VERSION_CPP != $TXS_VERSION_ISS1 ]]; then echo Error: Version mismatch: iss: $TXS_VERSION_ISS1 cpp: $TXS_VERSION_CPP; exit; fi
 if [[ $TXS_VERSION_CPP != $TXS_VERSION_ISS2 ]]; then echo Error: Version mismatch: iss 2: $TXS_VERSION_ISS2 cpp: $TXS_VERSION_CPP; exit; fi
 if [[ $TXS_VERSION_CPP != $TXS_VERSION_MANUAL ]]; then echo Error: Version mismatch: manual: $TXS_VERSION_MANUAL cpp: $TXS_VERSION_CPP; exit; fi
