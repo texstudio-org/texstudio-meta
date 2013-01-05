5be57683c48869c1260dceb9b3a1123788f8be84
benibela <benito@benibela.de>
2013-01-05 23:39:01 +0000

    package creation script for new svn
    git-svn-id: svn://svn.code.sf.net/p/texstudio/code/trunk@3642 92cce386-e96d-4106-809f-596c673a3fd8

--- createtag_srctar.sh
+++ createtag_srctar.sh
@@ -3,7 +3,14 @@
 echo "create svn tag and src tarball"
 
 
+if [[ ! (-e ./texstudio.pro) ]]; then echo need to be called in txs root; exit; fi 
 
+SFUSER="";
+if [ -e /home/benito ]; then SFUSER="benibela"; eval `keychain -q --eval --agents ssh id_rsa `; 
+else echo "Enter sourceforge user name:"; read SFUSER; fi
+if [ "x$SFUSER"  = "x" ]; then echo "no sf user"; exit; fi
+
+svn update
 
 
 TMXREVISION=`svn info | grep Revision | grep -oE [0-9]+`
@@ -20,33 +27,29 @@ TMXVERSION=$TXS_VERSION_CPP
 
 echo "Publishing version $TMXVERSION"
 
-SFUSER="";
-if [ -e /home/benito ]; then SFUSER="benibela"; eval `keychain -q --eval --agents ssh id_rsa `; 
-else echo "Enter sourceforge user name:"; read SFUSER; fi
-if [ "x$SFUSER"  = "x" ]; then echo "no sf user"; exit; fi
-
 
 
 svn rm --keep-local  -m "remove possible broken tag" svn+ssh://$SFUSER@svn.code.sf.net/p/texstudio/code/tags/TeXstudio/$TMXVERSION
 
-svn copy -r $TMXREVISION  -m "TAGGED $TMXVERSION RELEASE" svn+ssh://$SFUSER@svn.code.sf.net/p/texstudio/code/trunk svn+ssh://$SFUSER@svn.code.sf.net/p/texstudio/code/tags/TeXstudio/$TMXVERSION
+RELEASESVNPATH=`svn info | grep URL | grep -oE "svn.*"`
+
+
+svn copy -r $TMXREVISION  -m "TAGGED $TMXVERSION RELEASE" $RELEASESVNPATH svn+ssh://$SFUSER@svn.code.sf.net/p/texstudio/code/tags/TeXstudio/$TMXVERSION
 
 
 
 #get tar from svn
 rm -Rf /tmp/tmxsrc /tmp/tmxsrc2
-mkdir -p /tmp/tmxsrc
-wget  http://texstudio.svn.sf.net/viewvc/texstudio/tags/TeXstudio/$TMXVERSION/?view=tar -O /tmp/tmxsrc/tmx.tar.gz
+mkdir /tmp/tmxsrc2
+svn export . /tmp/tmxsrc2/texstudio$TMXVERSION
 
 #add svn_revision.cpp
-mkdir -p /tmp/tmxsrc2
-tar -xf /tmp/tmxsrc/tmx.tar.gz -C /tmp/tmxsrc2
 #cp svn_revision.cpp /tmp/tmxsrc2/$TMXVERSION/  
-echo "const char* TEXSTUDIO_SVN_VERSION = \"$TMXREVISION\";" > /tmp/tmxsrc2/$TMXVERSION/svn_revision.cpp
-mv /tmp/tmxsrc2/$TMXVERSION/ /tmp/tmxsrc2/texstudio$TMXVERSION
+echo "const char* TEXSTUDIO_SVN_VERSION = \"$TMXREVISION\";" > /tmp/tmxsrc2/texstudio$TMXVERSION/svn_revision.cpp
 
 #create
 cd /tmp/tmxsrc2
+mkdir /tmp/tmxsrc
 tar -czf /tmp/tmxsrc/texstudio-$TMXVERSION.tar.gz  texstudio$TMXVERSION
 
 cd /tmp/tmxsrc/
