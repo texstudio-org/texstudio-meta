fc420a3c717653b5ba77eb3bdcb58c1556758797
benibela <benito@benibela.de>
2013-10-21 00:29:25 +0200

    change upload script to commit to hg

--- webpage/upload.sh
+++ webpage/upload.sh
@@ -6,4 +6,6 @@ if [ "x$SFUSER"  = "x" ]; then echo "no sf user"; exit; fi
 
 rsync -v -e ssh index.html "$SFUSER,texstudio@web.sourceforge.net:/home/groups/t/te/texstudio/htdocs"
 
-svn commit -m "web page $@" index.html 
+#svn commit -m "web page $@" index.html
+hg commit -m "web page $*" index.html  
+hg push
