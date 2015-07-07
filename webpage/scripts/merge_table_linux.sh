0423ae783078ce23156979cffff99072a757ebc5
benibela <benito@benibela.de>
2015-07-07 23:34:17 +0200

    fix geometric formula, remove reference to sourceforge from logo

--- webpage/scripts/merge_table_linux.sh
+++ webpage/scripts/merge_table_linux.sh
@@ -12,3 +12,16 @@ xidel index.html -e 'xquery version "1.0";
   };
   local:update(/html)' --output-format html > /tmp/new.html
  cp /tmp/new.html index.html
+ 
+ exit
+ 
+ xidel index.html -e 'xquery version "3.0";
+  declare variable $newtablerows := doc("file:///tmp/table.html")//tr;
+  transform(function($e){
+    if ($e instance of element() and name($e) eq "tbody" and $e/@id eq "linuxdownloads") then
+     <tbody id="linuxdownloads">
+     {$newtablerows}
+     </tbody>
+    else $e
+  })' --output-format html > /tmp/new.html
+
