0f990422b6a8466567a989e2b77b6592ffd224f1
benibela <benito@benibela.de>
2015-09-05 11:31:44 +0200

    auto upload to ftp

--- webpage/upload.sh
+++ webpage/upload.sh
@@ -9,3 +9,13 @@ rsync -v -e ssh index.html "$SFUSER,texstudio@web.sourceforge.net:/home/groups/t
 #svn commit -m "web page $@" index.html
 hg commit -m "web page $*" index.html  
 hg push
+
+remotepath=/
+uploadCommands="put index.html"
+
+OWNDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $OWNDIR/.ftpdata
+uploadCommands="user $username $password\ncd $remotepath\n$uploadCommands\nbye";
+
+echo -e "$uploadCommands" | ftp -n -v $domain 
+
