0eebb13b0f150bbfd9c86f4d362682c4be917d1d
Tim Hoffmann <thoffmann@texstudio.org>
2016-10-11 23:20:24 +0200

    improved tar creation script

--- createsrctar1.sh
+++ createsrctar1.sh
@@ -1,46 +1,35 @@
 #/bin/bash
 
+if [[ ! (-e ./texstudio.pro) ]]; then echo "ERROR: This script needs to be called in the texstudio-hg root directory."; exit; fi 
+
 OWNDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $OWNDIR/checkversion.sh
+TXS_VERSION=$TXS_VERSION_CPP
+HG_VERSION=$(hg identify -n)
+HG_REVISION=$(hg identify -i)
 
+echo "creating src tarball for TeXstudio $TXS_VERSION hg $HG_VERSION:$HG_REVISION"
 
-echo "create src tarball"
-
-
-if [[ ! (-e ./texstudio.pro) ]]; then echo need to be called in txs root; exit; fi 
-
-SFUSER="";
-if [ -e /home/benito ]; then SFUSER="benibela"; eval `keychain -q --eval --agents ssh id_rsa `; 
-else echo "Enter sourceforge user name:"; read SFUSER; fi
-if [ "x$SFUSER"  = "x" ]; then echo "no sf user"; exit; fi
-
-#hg pull
-#hg update
+# update hg_revision.cpp
 ./hg_revision.sh echo > /dev/null
 
 
-#TMXREVISION=`svn info | grep Revision | grep -oE [0-9]+`
-#echo "Assuming i should tag revision $TMXREVISION"
-
-
-
-
-#echo "Enter new txs version:"
-#read TMXVERSION
-TMXVERSION=$TXS_VERSION_CPP
-
-#echo "Publishing version $TMXVERSION"
+# get sourceforge user
+SFUSER="";
+if [ -e /home/benito ]; then SFUSER="benibela"; eval `keychain -q --eval --agents ssh id_rsa `; 
+else echo "Please enter sourceforge user name:"; read SFUSER; fi
+if [ "x$SFUSER"  = "x" ]; then echo "ERROR: no sourceforge user"; exit; fi
 
 rm -rf /tmp/tmxsrc2
 mkdir /tmp/tmxsrc /tmp/tmxsrc2
 
 
-hg archive -v /tmp/tmxsrc2/texstudio$TMXVERSION
-cp hg_revision.cpp /tmp/tmxsrc2/texstudio$TMXVERSION
+hg archive -v /tmp/tmxsrc2/texstudio$TXS_VERSION
+cp hg_revision.cpp /tmp/tmxsrc2/texstudio$TXS_VERSION
 
 cd /tmp/tmxsrc2
 
 
-tar -cvzf /tmp/tmxsrc/texstudio-$TMXVERSION.tar.gz  texstudio$TMXVERSION
+tar -cvzf /tmp/tmxsrc/texstudio-$TXS_VERSION.tar.gz  texstudio$TXS_VERSION
 cd /tmp/tmxsrc/
-rsync -avRP -e ssh texstudio-$TMXVERSION.tar.gz "$SFUSER,texstudio@frs.sourceforge.net:/home/frs/project/t/te/texstudio/texstudio/TeXstudio\ $TMXVERSION/"
+rsync -avRP -e ssh texstudio-$TXS_VERSION.tar.gz "$SFUSER,texstudio@frs.sourceforge.net:/home/frs/project/t/te/texstudio/texstudio/TeXstudio\ $TXS_VERSION/"
