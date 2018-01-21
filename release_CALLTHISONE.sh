f67296e37d13f63aa5de9c40efcd08f1c7e516fd
sunderme@gmx.de <sunderme@gmx.de>
2018-01-21 13:19:57 +0100

    disabled steps which are no longer useful

--- release_CALLTHISONE.sh
+++ release_CALLTHISONE.sh
@@ -4,12 +4,12 @@ if [[ -e /home/benito ]]; then eval `keychain -q --eval --agents ssh id_rsa `; f
 
 OWNDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $OWNDIR/checkversion.sh
-$OWNDIR/createsrctar1.sh 
-$OWNDIR/createpkg.sh --release
+# $OWNDIR/createsrctar1.sh 
+# $OWNDIR/createpkg.sh --release
 $OWNDIR/create_releasenotes3.sh 
 echo uploaded. wait a while for platform defaults
 read 
-$OWNDIR/setplatformdefaults.sh 
+# $OWNDIR/setplatformdefaults.sh 
 cd $OWNDIR/webpage
 scripts/update_links_CALLTHISONE.sh $TXS_VERSION_CPP
 scripts/update_update.sh $TXS_VERSION_CPP
