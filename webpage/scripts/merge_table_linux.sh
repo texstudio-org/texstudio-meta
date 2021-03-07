01c6f1d11d8fb2c805af07a363962aaf44746ab9
Benito van der Zander <benito@benibela.de>
2021-03-07 19:08:21 +0100

    fix script for new xidel version

--- webpage/scripts/merge_table_linux.sh
+++ webpage/scripts/merge_table_linux.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-xidel index.html -e 'xquery version "3.0-xidel";
+xidel index.html -e 'xquery version "3.1-xidel";
   declare variable $newtablerows := doc("file:///tmp/table.html")//tr;
-  transform(function($e){
-    if ($e instance of element() and name($e) eq "tbody" and $e/@id eq "linuxdownloads") then
+  x:replace-nodes(//tbody[@id eq "linuxdownloads"],
      <tbody id="linuxdownloads">
      {$newtablerows}
      </tbody>
-    else $e
-  })' --output-format html > /tmp/new.html
- cp /tmp/new.html index.html
+  )' --output-format html > /tmp/new.html && cp /tmp/new.html index.html
  
  
 
