8d8de1178fa5e6ab7b7a2782ad199932fcb8a5df
Jan Sundermeyer <sunderme@gmx.de>
2021-10-23 13:53:37 +0200

    merge linux table if present

--- webpage/scripts/update_version.pl
+++ webpage/scripts/update_version.pl
@@ -5,21 +5,40 @@ use warnings;
 
 my $newVersion=$ARGV[0];
 
-
-print $newVersion;
+# read linux table if exits
+my $TH;
+if( -e "/tmp/table.html" ){
+  open($TH, '<', '/tmp/table.html');
+}
 
 my $filename = 'index.html';
 
 open(FH, '<', $filename) or die $!;
+my $doPrint=1;
 
 while(<FH>){
-   if( m/releases\/download/ ){
-     s/\d\.\d+\.\d+/$newVersion/g;
-   }
-   if( m/currentVersion/ ){
-     s/\d\.\d+\.\d+/$newVersion/g;
-   }
-   print $_;
+  if( m/releases\/download/ ){
+    s/\d\.\d+\.\d+/$newVersion/g;
+  }
+  if( m/currentVersion/ ){
+    s/\d\.\d+\.\d+/$newVersion/g;
+  }
+  if( !$doPrint && m/\/tbody/){
+    $doPrint=1;
+  }
+  if( m/tbody.*linuxdownloads/){
+    print $_;
+    if(defined $TH){
+      $doPrint=0;
+      while(<$TH>){
+        if( m/<.?table>/){ 
+          next;
+        }
+        print $_;
+      }
+    }
+  }
+  print $_ if $doPrint;
 }
 
 close(FH);
